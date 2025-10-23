69b7bfa0e0b1bc8a6a2bdc1e1452ef151c50f93d
Javier Baena Martin <jbaemar@upv.edu.es>
2025-10-24 00:18:39 +0200

    otro backup

--- ListArray.h
+++ ListArray.h
@@ -1,10 +1,11 @@
 #include <iostream>
 #include <ostream>
+#include <stdexcept>
 #include "List.h"
 using namespace std;
 
 template<typename T>
-class ArrayList : public List<T> {
+class ListArray : public List<T> {
 	private:
 		T* arr;//puntero al inicio del array.
 		int max;//n elem maximos 
@@ -23,64 +24,87 @@ class ArrayList : public List<T> {
 
 	public:
 		ListArray(){
+			arr = new T[MINSIZE];
+			max = MINSIZE;
+			n = 0;
+		}
 
 
+		~ListArray(){
+			delete[] arr;
 		}
 		
-		~ListArray(){
-
-
-		}
-
 		void insert(int pos, T e) override{
-
-
+			if(pos < 0 || pos > n-1){
+				throw out_of_range("Posicion no valida");
+			}
+			if(n == max){
+				resize(max*2);
+			}
+			for(int i = n; i >pos; i--){
+				arr[i] = arr[i-1];
+			}
+			arr[pos] = e;
+			n++;
 		}
 
 		void append(T e) override{
-
+			if(n == max){
+				resize(max*2);
+			}
+			arr[n] = e;
+			n++;
 
 		}
                 
 		void prepend(T e) override{
-
-
-
+			if(n== max){
+				resize(max*2);
+			}
+			for(int i = n; i > 0; i-- ){
+				arr[i]=arr[i-1];
+			}
+			arr[0] = e;
 		}
 
-                T remove(int pos) override{
-
-
-
+        T remove(int pos) override{
+			if(pos < 0 || pos > n-1){
+				throw out_of_range("Posicion no valida");
+			}
+			T removed = arr[pos];
+			for(int i = pos; i < n-1; i++){
+				arr[i] = arr[i+1];
+			}
+			n--;
+			return removed;
 		}
 
-                T get(int pos) override{
-
-
-
+        T get(int pos) override{
+			if(pos < 0 || pos > n-1){
+				throw out_of_range("Posicion no valida");
+			}
+			return arr[pos];
 		}
 
-                int search(T e) override{
-
-
-
+        int search(T e) override{
+			for(int i = 0; i < n; i++){
+				if(arr[i] == e){
+					return i;
+				}
+			}
+			return -1;
 		}
 
-                bool empty() override{
-
-
-
+        bool empty() override{
+			return n == 0;
 		}
 
-                int size() override{
-
-
-
+		int size() override{
+			return n;
 		}
 		
 		T operator[](int pos){
-
-
+		
 		}
 
 		friend std::ostream& operator<<(std::ostream &out, const ListArray<T> &list){
